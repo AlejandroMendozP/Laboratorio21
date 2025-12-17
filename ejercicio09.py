977f317d077993ba5ae2c12aaa65f807dfc87439
Alejandro <amendozapan@unsa.edu.pe>
2025-12-16 20:33:38 -0500

    Ejercicio 9.3

--- ejercicio09.py
+++ ejercicio09.py
@@ -1,28 +1,27 @@
+import multiprocessing
 import time
-import asyncio
 import random
 
-async def consultar(archivo, tiempo):
-    print(f"Consultando...{archivo}")
-    await asyncio.sleep(tiempo)
-    print(f"Consulta exitosa: {archivo}")
-
+def consultar(archivo, tiempo):
+   print(f"Consultando...{archivo}")
+   time.sleep(tiempo)
+   print(f"Finalizó {archivo}")
+   
 tareas = [
     ("Archivo A", random.uniform(1, 5)),
     ("Archivo B", random.uniform(1, 5)),
     ("Archivo C", random.uniform(1, 5))
 ]
 
-inicio = time.time()
-
-async def main():
+if __name__ == "__main__":
    inicio = time.time()
-   await asyncio.gather(
-       consultar("Archivo A", random.uniform(1, 5)),
-       consultar("Archivo B", random.uniform(1, 5)),
-       consultar("Archivo C", random.uniform(1, 5))
+   procesos = []
+   for nombre, t in tareas:
+    p = multiprocessing.Process(target=consultar, args=(nombre, t))
+    p.start()
+    procesos.append(p)
     
-    )
-   
-print("Tiempo total:", time.time() - inicio)
-asyncio.run(main())
+    for p in procesos:
+       p.join()
+    
+    print("Tiempo total:", time.time() - inicio)
